dec599eff5cee6d9d11a8b1ddc6a2ed68390a456
Eliza Weisman <eliza@buoyant.io>
2020-01-18 11:00:18 -0800

    now with '100% Safe Rust'
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,28 @@ pub(crate) mod loom;
 
 use crate::loom::{
     atomic::{AtomicUsize, Ordering},
-    CausalCell,
+    Arc, CausalCell, Mutex, Weak,
 };
+
+pub fn with_capacity(capacity: usize) -> (Writer, Reader) {
+    let mut buf = Vec::with_capacity(capacity);
+    buf.resize_with(capacity, || Mutex::new(String::new()));
+    let buf = buf.into_boxed_slice();
+    let inner = Arc::new(Inner {
+        r_i: AtomicUsize::new(0),
+        w_i: AtomicUsize::new(0),
+        len: capacity,
+        buf,
+    });
+
+    let reader = Reader {
+        inner: Arc::downgrade(&inner),
+        next: 0,
+    };
+    let writer = Writer { inner };
+
+    (writer, reader)
+}
 
 #[derive(Clone)]
 pub struct Writer {
@@ -19,22 +39,26 @@ struct Inner {
     r_i: AtomicUsize,
     w_i: AtomicUsize,
     len: usize,
-    buf: [CausalCell<String>],
+    buf: Box<[Mutex<String>]>, // XXX(eliza): i hate the second box...
 }
 
 impl Writer {
     pub fn write<T>(&self, f: impl FnOnce(&mut String) -> T) -> T {
-        let this = *self.inner;
+        let this = &*self.inner;
         // XXX(eliza): there is maybe a bug here if writes on other threads
         // "lap" us while we are still writing...tbqh, we could protect against
         // this w/ a mutex...
-        let idx = this.w_i.fetch_add(1, Ordering::AcqRel);
-        // we now exclusively own `idx`
-        let res = this.buf[idx % this.len].with_mut(|s| {
-            let s = unsafe { &mut *s };
-            s.clear();
-            f(s)
-        });
+        let w = this.w_i.fetch_add(1, Ordering::AcqRel);
+        let idx = w % this.len;
+        // we now exclusively own `idx` (unless someone laps us)...
+        #[cfg(debug_assertions)]
+        let mut lock = this.buf[idx]
+            .try_lock()
+            .expect("someone lapped us, slow the heck down!");
+        #[cfg(not(debug_assertions))]
+        let mut lock = this.buf[idx].lock().unwrap();
+
+        let res = f(&mut *lock);
         // scootch read index
         this.r_i.fetch_add(1, Ordering::Release);
         res
@@ -48,17 +72,21 @@ pub struct Closed {
 impl Reader {
     /// Returns `None`
     pub fn read<T>(&mut self, f: impl FnOnce(&String) -> T) -> Result<Option<T>, Closed> {
-        let this = Arc::upgrade(self.inner).ok_or(Closed { _p: () })?;
+        let this = self.inner.upgrade().ok_or(Closed { _p: () })?;
         let read_ix = this.r_i.load(Ordering::Acquire);
         if self.next >= read_ix {
             // gotta slow down!
             return Ok(None);
         }
+        let idx = self.next % this.len;
 
-        let res = this.buf[self.next % this.len].with(|s| {
-            let s = unsafe { &*s };
-            f(s)
-        });
+        #[cfg(debug_assertions)]
+        let lock = this.buf[idx]
+            .try_lock()
+            .expect("unless poisoned, this should always succeed???");
+        #[cfg(not(debug_assertions))]
+        let lock = this.buf[idx].lock().unwrap();
+        let res = f(&*lock);
 
         self.next += 1;
         Ok(Some(res))
--- src/loom.rs
+++ src/loom.rs
@@ -3,6 +3,7 @@ pub(crate) use self::inner::*;
 #[cfg(test)]
 mod inner {
     pub(crate) use loom::sync::CausalCell;
+    pub(crate) use loom::sync::Mutex;
     pub(crate) use loom::sync::{Arc, Weak};
     pub(crate) mod atomic {
         pub use loom::sync::atomic::*;
@@ -14,6 +15,7 @@ mod inner {
 mod inner {
     use std::cell::UnsafeCell;
     pub(crate) use std::sync::atomic;
+    pub(crate) use std::sync::Mutex;
     pub(crate) use std::sync::{Arc, Weak};
 
     #[derive(Debug)]
